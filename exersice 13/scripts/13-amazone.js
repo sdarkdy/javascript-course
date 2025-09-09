38bbdb23083e69bdc6a6f90d235ab745a4ee5c54
sdarkdy <sdarkdy.91@outlook.com>
2025-09-09 08:33:30 +0400

    cart module edited

--- exersice 13/scripts/13-amazone.js
+++ exersice 13/scripts/13-amazone.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-              <select>
+              <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -69,12 +69,18 @@ document.querySelectorAll('.js-add-to-cart')
   }
  });
 
- if (matchingItem) {
-  matchingItem.quantity += 1;
- } else {
-    cart.push({
-    productId: productId,
-    quantity: 1
+ const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      const quantity = quantitySelector.value;
+      console.log(quantity)
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+      cart.push({
+      productId: productId,
+      quantity: 1
     });
  }
 
